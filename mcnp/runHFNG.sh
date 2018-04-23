ad3d32fd8cd0e1546d4b94cd0e4eca0464d39fc8
Mitch Negus <mitchell.negus.57@gmail.com>
2018-04-23 16:16:58 -0700

    created runscript for savio

---
+++ mcnp/runHFNG.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=HFNG-Edist
+# Account:
+#SBATCH --account=fc_deans
+# Partition:
+#SBATCH --partition=savio
+# QoS:
+#SBATCH --qos=savio_normal
+# Number of nodes needed for use case:
+#SBATCH --nodes=1
+# Tasks per node based on number of cores per node (example):
+#SBATCH --ntasks-per-node=20
+# Processors per task:
+#SBATCH --cpus-per-task=1
+# Wall clock limit:
+#SBATCH --time=00:30:00
+## Command(s) to run (example):
+module load gcc openmpi
+mpirun -np 20 mcnp6.mpi  i=HFNG.inp o=HFNG.out
