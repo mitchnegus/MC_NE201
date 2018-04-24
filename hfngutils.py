f46831ffef0ec7a01ae1a5e4d4190a90633bf538
Mitch Negus <mitchell.negus.57@gmail.com>
2018-04-24 10:44:44 -0700

    added utilities file for use in python scripts

---
+++ hfngutils.py
@@ -0,0 +1,13 @@
+"""Utilities for python scripts'''
+
+def read_mcnp_input(filename):
+    '''Read MCNP input and return a list of lines, separated by item'''
+    with open(filename,'r') as infile:
+        linelist = [line.rstrip('\n& ').split() for line in infile.readlines()]
+    return linelist
+
+def read_mcnp_output(filename):
+    '''Read MCNP output and return a list of lines, separated by item'''
+    with open(filename,'r') as infile:
+        linelist = [line.rstrip('\n ').split() for line in infile.readlines()]
+    return linelist
