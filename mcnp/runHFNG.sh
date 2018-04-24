a33bc081f0976cc9d4a22d36581951164fc64fcf
Mitch Negus <mitchell.negus.57@gmail.com>
2018-04-24 16:42:45 -0700

    completed run; finer binning, more particles

--- mcnp/runHFNG.sh
+++ mcnp/runHFNG.sh
@@ -8,15 +8,15 @@
 # QoS:
 #SBATCH --qos=savio_normal
 # Number of nodes needed for use case:
-#SBATCH --nodes=4
+#SBATCH --nodes=20
 # Tasks per node based on number of cores per node (example):
 #SBATCH --ntasks-per-node=20
 # Processors per task:
 #SBATCH --cpus-per-task=1
 # Wall clock limit:
-#SBATCH --time=00:10:00
+#SBATCH --time=00:30:00
 ## Command(s) to run (example):
 date
 module load intel openmpi
-mpirun -np 80 mcnp6.mpi  i=HFNG.inp o=HFNG.out
+mpirun -np 400 mcnp6.mpi  i=HFNG.inp o=HFNG.out
 date
