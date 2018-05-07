788bec8bc2442c6ecb3aa2f284e6df7075fae475
Mitch Negus <mitchell.negus.57@gmail.com>
2018-05-07 15:22:00 -0700

    runscript for Jon's run

---
+++ mcnp/runHFNG-jm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=HFNG-Edist
+# Account:
+#SBATCH --account=fc_deans
+# Partition:
+#SBATCH --partition=savio
+# QoS:
+#SBATCH --qos=savio_normal
+# Number of nodes needed for use case:
+#SBATCH --nodes=20
+# Tasks per node based on number of cores per node (example):
+#SBATCH --ntasks-per-node=20
+# Processors per task:
+#SBATCH --cpus-per-task=1
+# Wall clock limit:
+#SBATCH --time=01:00:00
+## Command(s) to run (example):
+date
+module load intel openmpi
+mpirun -np 400 mcnp6.mpi  i=HFNG-jm.inp o=HFNG.out
+date
